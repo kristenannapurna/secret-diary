e3145cf00b67506dbb97a72862dbcd58f26fb2be
Kristen <kristen@kristenspencer.ca>
2018-12-04 09:02:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newEntry: "",
+      diaryEntries: {}
+    };
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    const newDiaryEntry = {
+      date: new Date().toDateString(),
+      body: this.state.newEntry
+    };
+
+    this.setState({
+      newEntry: ""
+    });
+
+    // push data to firebase here
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+          <h1>My ✨Special✨ Special✨ Secret Diary 🙊</h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.newEntry}
+              id="newEntry"
+              onChange={this.handleChange}
+            />
+          </form>
+          <input type="submit" />
+          <section className="entries">
+            {Object.entries(this.state.diaryEntries).map(entry => {
+              return (
+                <article key={entry[0]}>
+                  <p>Written on: {entry[1].date}</p>
+                  <p>{entry[1].body}</p>
+                </article>
+              );
+            })}
+          </section>
+        </main>
       </div>
     );
   }
